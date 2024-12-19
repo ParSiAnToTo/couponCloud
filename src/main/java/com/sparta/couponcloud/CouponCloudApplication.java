433c0ea474cc3b707da4b6ddda6dcffad5e415c9
ParSiAnToTo <wushuchj@gmail.com>
2024-12-19 17:31:27 +0900

    init: project init

---
+++ src/main/java/com/sparta/couponcloud/CouponCloudApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.couponcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/couponcloud/CouponCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.couponcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
